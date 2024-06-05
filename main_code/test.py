e84ac02268069b4b5476a99ce7c945bdba00f815
056wagomu <72100310+056wagomu@users.noreply.github.com>
2024-06-06 00:36:05 +0900

    modified:   .devcontainer/devcontainer.json 	new file:   main_code/crabnet_basic_colab.ipynb 	new file:   main_code/figures/lc_data/UnnamedModel_lc.csv 	new file:   main_code/figures/lc_data/UnnamedModel_lc.png 	new file:   main_code/models/trained_models/UnnamedModel.pth 	modified:   main_code/test.py

--- main_code/test.py
+++ main_code/test.py
@@ -1,3 +1,8 @@
+from gensim.models import Word2Vec
 from mat2vec.processing import MaterialsTextProcessor
+
+w2v_model = Word2Vec.load(".venv/lib/python3.7/site-packages/mat2vec/training/models/pretrained_embeddings")
 text_processor = MaterialsTextProcessor()
-text_processor.process("LiCoO2 is a battery cathode material.")
+answer = w2v_model.wv.most_similar("selenium", topn=5)
+
+print(answer)
