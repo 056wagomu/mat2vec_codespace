69d52bf6b650f1ade2a9abcdf5023619e4f9cb58
056wagomu <72100310+056wagomu@users.noreply.github.com>
2024-06-24 02:28:56 +0900

    new file:   main_code/candidate_material_list/B_candidate_material_list.csv 	new file:   main_code/candidate_material_list/Ba_candidate_material_list.csv 	new file:   main_code/candidate_material_list/Ni_candidate_material_list.csv 	new file:   main_code/candidate_material_list/Sr_candidate_material_list.csv 	new file:   main_code/cbnet_model_test.py 	renamed:    main_code/models/trained_models/UnnamedModel.pth -> main_code/models/trained_models/SC_model_2nd_4head.pth 	new file:   main_code/predicted_Tc.csv 	renamed:    main_code/crabnet_basic_colab.ipynb -> main_code/trash_folder/crabnet_basic_colab.ipynb

---
+++ main_code/cbnet_model_test.py
@@ -0,0 +1,91 @@
+# %%
+#ライブラリ
+from os.path import join
+import os
+from pathlib import Path
+from typing import List
+
+import numpy as np
+import pandas as pd
+from crabnet.crabnet_ import CrabNet
+from crabnet.kingcrab import SubCrab
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import (
+    GroupKFold,
+    GroupShuffleSplit,
+)
+pd.set_option('display.max_rows', 1000)
+import torch
+
+from crabnet.utils.utils import (
+    Scaler
+)
+
+
+
+# %%
+X = []
+
+for csv in os.listdir("/workspaces/mat2vec_codespace/main_code/candidate_material_list"):
+    X.append(pd.read_csv("/workspaces/mat2vec_codespace/main_code/candidate_material_list/" + csv))
+    
+X = pd.concat(X, axis=0)
+
+
+#X = pd.read_csv("/workspaces/mat2vec_codespace/main_code/candidate_material_list/B_candidate_material_list.csv")
+
+input_df = pd.DataFrame(
+    {
+        "formula": X["new_formula"],
+        "efermi" : X["efermi"],
+        "band_gap": X["band_gap"],
+        "target": None
+    }
+)
+
+input_df = input_df.reset_index(drop=True)
+
+display(input_df)
+
+
+# %%
+model_state_dict = torch.load("/workspaces/mat2vec_codespace/main_code/models/trained_models/SC_model_2nd_4head.pth")
+#print(model_state_dict.keys())
+
+
+cbnet = CrabNet()
+cbnet.model = SubCrab()
+cbnet.scaler = Scaler(torch.zeros(3))
+
+
+
+# %%
+cbnet.model.load_state_dict(model_state_dict["weights"])
+
+cbnet.scaler.load_state_dict(model_state_dict["scaler_state"])
+cbnet.model_name = model_state_dict["model_name"]
+
+
+# %%
+y_pred= cbnet.predict(test_df = input_df)
+
+
+# %%
+
+pred_df = pd.DataFrame(
+    {
+        "formula": X["new_formula"],
+        "predicter_Tc": y_pred,
+    }
+)
+
+display(pred_df)
+
+
+# %%
+pred_df = pred_df.sort_values("predicter_Tc", ascending=False)
+# %%
+display(pred_df.head(100))
+# %%
+pred_df.to_csv("/workspaces/mat2vec_codespace/main_code/predicted_Tc.csv", index=False)
+# %%
