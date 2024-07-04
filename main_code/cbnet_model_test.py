68ac3f5bfe0041a6034aa596f694435a69472823
056wagomu <72100310+056wagomu@users.noreply.github.com>
2024-07-04 15:49:49 +0900

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   main_code/candidate_stable_material_list/Al_is_stable_candidate_material_list.csv
    	modified:   main_code/candidate_stable_material_list/B_is_stable_candidate_material_list.csv
    	modified:   main_code/candidate_stable_material_list/C_is_stable_candidate_material_list.csv
    	new file:   main_code/candidate_stable_material_list/Ca_is_stable_candidate_material_list.csv
    	new file:   main_code/candidate_stable_material_list/K_is_stable_candidate_material_list.csv
    	modified:   main_code/candidate_stable_material_list/Li_is_stable_candidate_material_list.csv
    	new file:   main_code/candidate_stable_material_list/Mg_is_stable_candidate_material_list.csv
    	modified:   main_code/candidate_stable_material_list/Ni_is_stable_candidate_material_list.csv
    	modified:   main_code/candidate_stable_material_list/Si_is_stable_candidate_material_list.csv
    	modified:   main_code/cbnet_model_test.py
    	modified:   main_code/models/trained_models/SC_model_3rd_4head.pth
    	deleted:    main_code/predicted_Tc.csv
    	new file:   main_code/predicted_Tc_by2nd.csv
    	new file:   main_code/predicted_Tc_by2nd_final.csv
    	new file:   main_code/predicted_Tc_by3rd.csv
    	new file:   main_code/predicted_Tc_by3rd_final.csv
    	deleted:    main_code/predicted_Tc_filterd.csv
    	new file:   main_code/predicted_Tc_filterd_by2nd.csv
    	new file:   main_code/predicted_Tc_filterd_by2nd_final.csv
    	new file:   main_code/predicted_Tc_filterd_by3rd.csv
    	new file:   main_code/predicted_Tc_filterd_by3rd_final.csv

--- main_code/cbnet_model_test.py
+++ main_code/cbnet_model_test.py
@@ -30,22 +30,75 @@ for csv in os.listdir("/workspaces/mat2vec_codespace/main_code/candidate_stable_
     X.append(pd.read_csv("/workspaces/mat2vec_codespace/main_code/candidate_stable_material_list" + "/" + csv))
     
 X = pd.concat(X, axis=0)
+X = X.reset_index(drop=True)
 
+drop_idx = []
+#指定の有毒であったりする元素や銅酸化物系鉄系の化合物を削除
+H = "1"
+Cl = "17"
+As = "33"
+Se = "34"
+Br = "35"
+Tc = "43"
+Cd = "48"
+Sb = "51"
+Pb = "82"
+U = "92"
+Ra = "88"
+N = "7"
+Hg = "80"
+I = "53"
+remove_list = [H, Cl, As, Se, Br, Tc, Cd, Sb, Pb, U, Ra, N, Hg, I]
 
-#X = pd.read_csv("/workspaces/mat2vec_codespace/main_code/candidate_material_list/B_candidate_material_list.csv")
+for i in range(len(X)):
+    for remove in remove_list:
+        if remove in X.loc[i, "sorted_keys_comp_atoms"]:
+            drop_idx.append(i)
+            break
+    
+    if "O" in X.loc[i, "sorted_keys_comp_atoms"] and "Cu" in X.loc[i, "sorted_keys_comp_atoms"]:
+        drop_idx.append(i)
+        
+    if X.loc[i, "database_IDs"] == "False":
+        drop_idx.append(i)
+        
+    if "O" not in X.loc[i, "sorted_keys_comp_atoms"] and "Li" in X.loc[i, "sorted_keys_comp_atoms"]:
+        drop_idx.append(i)
+        
+drop_idx = list(set(drop_idx))
+        
+X = X.drop(drop_idx, axis=0)
+X = X.reset_index(drop=True)
 
 input_df = pd.DataFrame(
     {
         "formula": X["new_formula"],
         "efermi" : X["efermi"],
         "band_gap": X["band_gap"],
+        "database_IDs": X["database_IDs"],
         "target": None
     }
 )
 
-input_df = input_df.reset_index(drop=True)
-
 display(input_df)
+print(len(drop_idx))
+print(input_df.isnull().sum())
+
+
+
+# %%
+#ドープ後の結果を予測するためにデータフレームを自作する
+#Zn(Cu, Ga), Sn(In)の2つでドープ可能性サイトがある
+doped_info_1 = {"formula": "Li2Zn0.9Cu0.1Sn3O8", "efermi": 5.36169647, "band_gap": 1.9407, "target": None}
+doped_info_2 = {"formula": "Li2Zn0.9Ga0.1Sn3O8", "efermi": 5.36169647, "band_gap": 1.9407, "target": None}
+doped_info_3 = {"formula": "Li2Zn1Sn2.9In0.1O8", "efermi": 5.36169647, "band_gap": 1.9407, "target": None}
+doped_info_4 = {"formula": "Li2Cu0.1Ni0.9O3", "efermi": 2.36381262, "band_gap": 1.1037, "target": None}
+doped_info_5 = {"formula": "Li2Ni0.9Co0.1O3", "efermi": 2.36381262, "band_gap": 1.1037, "target": None}
+doped_info_6 = {"formula": "Li1V2.9Ti0.1Te2O12", "efermi": 2.06195658, "band_gap": 1.8778, "target": None}
+doped_info_7 = {"formula": "Li1V2.9Cr0.1Te2O12", "efermi": 2.06195658, "band_gap": 1.8778, "target": None}
+
+input_df_doped = pd.DataFrame([doped_info_1, doped_info_2, doped_info_3, doped_info_4, doped_info_5, doped_info_6, doped_info_7])
+display(input_df_doped)
 
 
 # %%
@@ -64,10 +117,11 @@ cbnet.model.load_state_dict(model_state_dict["weights"])
 
 cbnet.scaler.load_state_dict(model_state_dict["scaler_state"])
 cbnet.model_name = model_state_dict["model_name"]
+print(cbnet.model_name)
 
 
 # %%
-y_pred= cbnet.predict(test_df = input_df)
+y_pred= cbnet.predict(test_df = input_df_doped)
 
 
 # %%
@@ -77,23 +131,26 @@ pred_df = pd.DataFrame(
         "formula": X["new_formula"],
         "predicted_Tc": y_pred,
         "efermi" : X["efermi"],
-        "band_gap": X["band_gap"]
+        "band_gap": X["band_gap"],
+        "database_IDs": X["database_IDs"]
     }
 )
 
 pred_df = pred_df.sort_values("predicted_Tc", ascending=False)
 
+pred_df = pred_df.drop_duplicates()
+
 display(pred_df)
 
-# %%
-#pred_df.to_csv("/workspaces/mat2vec_codespace/main_code/predicted_Tc.csv", index=False)
+pred_df.to_csv("/workspaces/mat2vec_codespace/main_code/predicted_Tc_by3rd_final.csv", index=False)
 
 
 # %%
-#pred_df_filterd = pred_df.loc[pred_df["formula"].str.len() < 10, :].head(100)
-pred_df_filterd = pred_df.loc[(pred_df["band_gap"] < 3) & (pred_df["band_gap"] > 0), :].head(100)
+pred_df_filterd = pred_df.loc[(pred_df["band_gap"] < 3) & (pred_df["band_gap"] > 0), :]
 
 display(pred_df_filterd)
-#pred_df_filterd.to_csv("/workspaces/mat2vec_codespace/main_code/predicted_Tc_filterd.csv", index=False)
+pred_df_filterd.to_csv("/workspaces/mat2vec_codespace/main_code/predicted_Tc_filterd_by3rd_final.csv", index=False)
+
 
 # %%
+print(y_pred)
